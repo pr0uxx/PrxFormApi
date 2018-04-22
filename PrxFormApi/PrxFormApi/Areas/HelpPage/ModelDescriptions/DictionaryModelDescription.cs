555d71aa24d41a0647c519dd3ce3677e7f670ef9
pr0uxx <michow289@hotmail.com>
2018-04-22 19:52:27 +0100

    Base project

---
+++ PrxFormApi/PrxFormApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PrxFormApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ PrxFormApi/PrxFormApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PrxFormApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
